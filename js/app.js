c8d77b32bf6401af5f915bddf3af29faea7855aa
elizabethbodzy <ebodzy@gmail.com>
2017-09-05 15:02:49 -0400

    second of final site

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-// $('.js-slideshow').slick();
-// .css({
-// 	'position': 'absolute',
-// 	'top': '100px'
-// }).
-
-// const $slideshowWideawake = $('.js-slideshow-wideawake');
-
-// $slideshowWideawake.slick({
-//   infinite: true,
-//   arrows: false,
-
-// });
-
 const $slideshow = $('.js-slideshow');
 if ($slideshow.length > 0) {
 	$slideshow.slick({
@@ -33,43 +19,4 @@ if ($slideshow.length > 0) {
 	$nextButton.click(onNextButtonClick)
 	$previousButton.click(onPreviousButtonClick)
 }
-
-
-
-
-// const sandboxId = ''
-// const proId = ''
-
-//     paypal.Button.render({
-
-//         env: 'sandbox', // Or 'production for live'
-
-//         client: {
-//             sandbox:    sandboxId,
-//             production: proId,
-//         },
-
-//         commit: true, // Show a 'Pay Now' button
-
-//         payment: function(data, actions) {
-//             return actions.payment.create({
-//                 payment: {
-//                     transactions: [
-//                         {
-//                             amount: { total: '1.00', currency: 'USD' }
-//                         }
-//                     ]
-//                 }
-//             });
-//         },
-
-//         onAuthorize: function(data, actions) {
-//             return actions.payment.execute().then(function(payment) {
-
-//                 // The payment is complete!
-//                 // You can now show a confirmation message to the customer
-//             });
-//         }
-
-//     }, '#paypal-button');
 
